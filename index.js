8b492afd2d3f204635a60ebe13bbec2afe4b6af8
Nayan Jodhawat <nayanjodhawat@gmail.com>
2022-05-24 17:38:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World from Docker and Node.js!</h1>');
+    res.send('<h1>Hello World from Node.js!</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
